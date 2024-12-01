b2927144065ecd9529e43641bf06dd6074b3331e
larry901117 <larry901117@gmail.com>
2024-12-02 00:06:31 +0800

    Change to use command line function

--- whisper_tool.py
+++ whisper_tool.py
@@ -1,30 +1,32 @@
 import json
-import os, subprocess, pathlib
+import os, subprocess
+import sys
 import whisper
 from whisper.utils import get_writer
 import configparser
 
 config = configparser.ConfigParser()
 setting_file = os.path.dirname(os.path.abspath(__file__))+os.path.sep+"setting.ini"
-print(setting_file)
+print("Setting file :"+setting_file)
 config.read(setting_file,encoding="UTF-8")
 
-file_path = config.get('input','filename')
-print(file_path)
+# file_path = config.get('input','filename')
+# print(file_path)
 writer_format = json.loads(config.get('output','format'))
-print(writer_format)
+print("output format :"+ str(writer_format))
 
-dir_path = os.path.dirname(file_path)
-output_folder = dir_path + os.path.sep + config.get('output','output_folder_name')
-print(output_folder)
+
 
 def split_file(file, folder):
-    file_suffix = pathlib.Path(file).suffix
-    file_name = pathlib.Path(file).name.split(".")[0]
+    global file_suffix
+    file_suffix = os.path.basename(file).split(".")[1]
+    file_name = os.path.basename(file).split(".")[0]
+    # file_suffix = pathlib.Path(file).suffix
+    # file_name = os.path.basename(file).split(".")[0]
     os.makedirs(folder,exist_ok=True)
     split_seconds = config.get('input','split_file_seconds')
-    voice_splitter_coomand = f"ffmpeg -i {file} -f segment -segment_time {split_seconds} -c copy {folder+os.path.sep+file_name}_out%03d{file_suffix}"
-    print(voice_splitter_coomand)
+    voice_splitter_coomand = f"ffmpeg -i {file} -f segment -segment_time {split_seconds} -c copy {folder+os.path.sep+file_name}_out%03d.{file_suffix}"
+    print("Command : "+ voice_splitter_coomand)
     subprocess.call(voice_splitter_coomand)
 
 
@@ -51,17 +53,25 @@ def delete_files(folder,keyword):
 
 def combine_files(folder):
     output_folder = folder
+    data = ""
     for txt_file in os.listdir(output_folder):
         if ".txt" in txt_file:
             print(txt_file)
             with open(output_folder + os.sep + txt_file,"r",encoding="UTF-8") as txt_file_content:
                 data += txt_file_content.read()
     print(data)
-    with open(output_folder + os.sep +"summary.txt","w",encoding="UTF-8") as file:
+    file_name = os.path.basename(file_path).split(".")[0]
+    with open(output_folder + os.sep +f"{file_name}.txt","w",encoding="UTF-8") as file:
         file.write(data)
 
 if __name__ == "__main__":
+    file_path = sys.argv[1]
+    dir_path = os.path.dirname(file_path)
+    output_folder = dir_path + os.path.sep + config.get('output','output_folder_name')
+    print("output folder :"+output_folder)
+
     split_file(file_path,output_folder)
     transcribe_file(output_folder)
-    delete_files(output_folder,".WAV")
+    delete_files(output_folder,file_suffix)
     combine_files(output_folder)
+    delete_files(output_folder,"out")
